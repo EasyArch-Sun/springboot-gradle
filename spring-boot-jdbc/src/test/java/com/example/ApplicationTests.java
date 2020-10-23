7a8ba541717119d6bd3e512e803d9057a306e5e1
EasyArch-Sun <3268543958@qq.com>
2020-10-23 21:46:20 +0800

    spring-boot-jdbc

---
+++ spring-boot-jdbc/src/test/java/com/example/ApplicationTests.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import com.example.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTests {
+
+    @Resource
+    private RedisTemplate<String, User> redisTemplate;
+
+    @Test
+    public void test() throws Exception {
+
+        User user = new User(1, "xxx");
+        redisTemplate.opsForValue().set(user.getName(), user);
+        User user1 = new User(2, "yyy");
+        redisTemplate.opsForValue().set(user1.getName(), user1);
+
+        Assert.assertEquals(1, redisTemplate.opsForValue().get("xxx").getAge().intValue());
+        Assert.assertEquals(2, redisTemplate.opsForValue().get("yyy").getAge().intValue());
+
+
+    }
+}
